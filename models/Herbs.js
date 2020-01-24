67f67a8b11c4f9ff5e59ef1e1457f12ec89dc112
BeZen13 <55999473+BeZen13@users.noreply.github.com>
2020-01-23 18:21:08 -0700

    Add files via upload

---
+++ models/Herbs.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// blueprint for the herb form
+
+const herbsSchema = new Schema(
+    {
+        herbname: {
+            type: String,
+            required: true //this will be applied to the searches
+        },
+        location: {
+            type: String,
+            
+        },
+        uses: {
+            type: String,
+            required: true // this will be applied to the searches
+        },
+        other: {
+            type: Object
+        },
+        popularUse:{
+            type: Boolean,
+            required: true // this will be applied to top 20 most popular
+        }
+
+    }
+)
+
+module.exports = mongoose.model("Herbs", herbsSchema)
+
+//Hopefully this fulfills the needs of our Schema!
