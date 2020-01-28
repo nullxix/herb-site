f3eed6331866b3e51289318d15d5fa95f680bf76
Samuel Joseph Betzen <samuelbetzen@gmail.com>
2020-01-27 21:18:42 -0700

    Added some text to the welcome page

--- client/src/components/WelcomePage.js
+++ client/src/components/WelcomePage.js
@@ -4,8 +4,15 @@ function WelcomePage() {
     return (
         <div className="welcome-content">
             <h2>Welcome to Mothers Medicinal Library!</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris a diam maecenas sed. Malesuada fames ac turpis egestas sed tempus urna et. Imperdiet sed euismod nisi porta lorem mollis aliquam ut. Habitant morbi tristique senectus et netus et. Turpis cursus in hac habitasse platea dictumst quisque sagittis. In hendrerit gravida rutrum quisque non tellus. Non sodales neque sodales ut etiam sit amet nisl. Ornare aenean euismod elementum nisi quis eleifend. At lectus urna duis convallis convallis tellus id. Aliquam id diam maecenas ultricies mi eget. Vestibulum rhoncus est pellentesque elit. Vestibulum morbi blandit cursus risus at ultrices. Dui vivamus arcu felis bibendum ut tristique et egestas. Neque gravida in fermentum et sollicitudin ac orci. Sociis natoque penatibus et magnis dis parturient. Nulla posuere sollicitudin aliquam ultrices sagittis. Iaculis urna id volutpat lacus laoreet non. Eu sem integer vitae justo eget magna fermentum iaculis.</p>
-            <p>Dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu. Massa sed elementum tempus egestas. Morbi tristique senectus et netus et malesuada fames. Netus et malesuada fames ac. Fermentum iaculis eu non diam phasellus. Velit dignissim sodales ut eu sem. Massa eget egestas purus viverra accumsan in nisl nisi. Vitae turpis massa sed elementum tempus. In ornare quam viverra orci sagittis eu. Bibendum ut tristique et egestas quis ipsum suspendisse ultrices gravida. Phasellus vestibulum lorem sed risus. Sit amet risus nullam eget felis eget. Est lorem ipsum dolor sit. Arcu dui vivamus arcu felis bibendum ut tristique. Aliquet nibh praesent tristique magna sit amet. Sed tempus urna et pharetra pharetra massa massa ultricies. A cras semper auctor neque vitae tempus. Sed id semper risus in hendrerit gravida rutrum quisque. Egestas pretium aenean pharetra magna ac placerat vestibulum. Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi.</p>
+            <p className="welcome">Through-out history the power of herbs has helped the growth and wellness of humankind, and life in general on Earth!
+            The tried and true benefits of these plants and materials have cured and treated countless illness and diseases.
+            Even to today many of our medicines are derived from a plant source, although some benefit has been lost in translation and the over processing of thess plants.
+
+            </p>
+            <p>What lies before is a database of some very commonly used herbs, and the benefit of them. 
+                It is our hope that the power of herbs grace your life as they have provided much healing and energy to ours!
+                Blessings, and my the power of Herbs be with you!
+            </p>
         </div>
     )
 }
