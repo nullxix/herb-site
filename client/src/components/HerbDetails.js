4dec8e4a1bcb185fb2888982e15962fcb469fbe6
Nicholas Black <57681918+nullxix@users.noreply.github.com>
2020-01-29 13:16:27 -0700

    added labels

--- client/src/components/HerbDetails.js
+++ client/src/components/HerbDetails.js
@@ -1,11 +1,30 @@
 import React from "react"
 
 function HerbDetails(props) {
-    const {herbname, location, uses, other} = props
+    const {herbName, location, uses, other} = props
+    const locationLabel =() =>{
+        return(
+            <>
+            <hr/>
+            <h3>Location</h3>
+            <hr/>
+            </>
+        )
+    }
     return (
-        <div className="herb-details">
-            <h3>{herbname}</h3>
+        <div style={{
+            border: '12px solid black',
+            padding: '8px',
+            margin: '20px'
+        }}className="herb-details">
+            <h2>Herb Name</h2>
+            <hr/>
+            <h3>{herbName}</h3>
+            {location && locationLabel()}
             <p>{location}</p>
+            <hr/>
+            <h3>Uses</h3>
+            <hr/>
             <p>{uses}</p>
             <p>{other}</p>
         </div>
